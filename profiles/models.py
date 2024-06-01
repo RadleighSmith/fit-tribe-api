e923d00fc51d2a801a6ed8cc43a66391537236a7
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-01 14:16:41 +0000

    Add profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True)
+    bio = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    profile_image = models.ImageField(
+        upload_to='profile_pictures/', default='../default_profile_image_g43b7d'
+    )
+    cover_image = models.ImageField(
+        upload_to='cover_images/', default='../default_cover_image_d23fdc'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s Profile Page"
