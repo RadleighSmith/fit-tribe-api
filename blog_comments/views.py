4f33d83822efd5221735453209d0aadafea467d4
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-04 20:09:06 +0000

    Add BlogCommentList view abd BlogCommentDetail using generics

--- blog_comments/views.py
+++ blog_comments/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from ft_api.permissions import IsOwnerOrReadOnly
+from .models import BlogComment
+from .serializers import BlogCommentSerializer, BlogCommentDetailSerializer
 
-# Create your views here.
+class BlogCommentList(generics.ListCreateAPIView):
+    serializer_class = BlogCommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = BlogComment.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        
+class BlogCommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = BlogCommentDetailSerializer
+    queryset = BlogComment.objects.all()
