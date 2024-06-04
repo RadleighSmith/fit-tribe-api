4bb813235927f5998381e0d4839513af92986bb8
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-04 17:18:51 +0000

    Add Blog list view, urls and blog db migration

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Blog
+from .serializers import BlogSerializer
 
-# Create your views here.
+class BlogList(APIView):
+    def get(self, request):
+        blogs = Blog.objects.all()
+        serializer = BlogSerializer(
+            blogs, many=True, context={'request' : request}
+        )
+        return Response(serializer.data)
