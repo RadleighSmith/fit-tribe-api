98e9a20e258b4c5d5b70e4468e7b3f88a276c987
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-04 19:02:27 +0000

    Add BlogComment model

--- blog_comments/models.py
+++ blog_comments/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from blogs.models import Blog
 
-# Create your models here.
+class BlogComment(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+        
+    def __str__(self):
+        return self.comment
