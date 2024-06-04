8828851ced60013904dbf79cd0e48f80156e8570
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-04 17:07:33 +0000

    add banner and image validation to blogs

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -2,10 +2,70 @@ from rest_framework import serializers
 from .models import Blog
 
 class BlogSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source = 'owner.username')
+    owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source = 'owner.profile.id')
-    profile_image = serializers.ReadOnlyField(source = 'owner.profile.profile_image.url')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.profile_image.url')
+
+    def validate_banner(self, value):
+        max_size = 1024 * 1024 * 2  # 2 MB
+        max_width = 4096
+        max_height = 4096
+        min_width = 1200
+        min_height = 400
+        
+        if value.size > max_size:
+            raise serializers.ValidationError(
+                'Image size is larger than 2 MB, please try uploading a smaller image.'
+            )
+
+        if value.width > max_width:
+            raise serializers.ValidationError(
+                f'Image width is larger than {max_width} pixels, please upload a smaller image.'
+            )
+        if value.height > max_height:
+            raise serializers.ValidationError(
+                f'Image height is larger than {max_height} pixels, please upload a smaller image.'
+            )
+        if value.width < min_width:
+            raise serializers.ValidationError(
+                f'Image width is smaller than {min_width} pixels, please upload a larger image.'
+            )
+        if value.height < min_height:
+            raise serializers.ValidationError(
+                f'Image height is smaller than {min_height} pixels, please upload a larger image.'
+            )
+        return value
+
+    def validate_image(self, value):
+        max_size = 1024 * 1024 * 2  # 2 MB
+        max_width = 4096
+        max_height = 4096
+        min_width = 600
+        min_height = 400
+        
+        if value.size > max_size:
+            raise serializers.ValidationError(
+                'Image size is larger than 2 MB, please try uploading a smaller image.'
+            )
+
+        if value.width > max_width:
+            raise serializers.ValidationError(
+                f'Image width is larger than {max_width} pixels, please upload a smaller image.'
+            )
+        if value.height > max_height:
+            raise serializers.ValidationError(
+                f'Image height is larger than {max_height} pixels, please upload a smaller image.'
+            )
+        if value.width < min_width:
+            raise serializers.ValidationError(
+                f'Image width is smaller than {min_width} pixels, please upload a larger image.'
+            )
+        if value.height < min_height:
+            raise serializers.ValidationError(
+                f'Image height is smaller than {min_height} pixels, please upload a larger image.'
+            )
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -14,8 +74,8 @@ class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
         fields = [
-            'id', 'owner','is_owner', 'profile_id', 
-            'profile_image','title', 'content', 
+            'id', 'owner', 'is_owner', 'profile_id', 
+            'profile_image', 'title', 'content', 
             'created_at', 'updated_at', 'banner', 
-            'image', 
+            'image',
         ]
