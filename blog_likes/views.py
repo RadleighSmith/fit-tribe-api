835fb8e3265ab192c2610b5d1cb379efcbadd53d
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-04 20:36:12 +0000

    Add blog_likes views.py for list and detail views

--- blog_likes/views.py
+++ blog_likes/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from ft_api.permissions import IsOwnerOrReadOnly
+from blog_likes.models import BlogLike
+from blog_likes.serializers import BlogLikeSerializer
 
-# Create your views here.
+
+class BlogLikeList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = BlogLikeSerializer
+    queryset = BlogLike.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class BlogLikeDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = BlogLikeSerializer
+    queryset = BlogLike.objects.all()
