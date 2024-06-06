b75c7ee664c220cab0366a36a516a4a2b7135197
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-06 19:44:44 +0000

    add workout likes views

--- workout_likes/views.py
+++ workout_likes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from ft_api.permissions import IsOwnerOrReadOnly
+from workout_likes.models import WorkoutLike
+from workout_likes.serializers import WorkoutLikeSerializer
 
-# Create your views here.
+class WorkoutLikeList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = WorkoutLikeSerializer
+    queryset = WorkoutLike.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        
+class WorkoutLikeDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = WorkoutLikeSerializer
+    queryset = WorkoutLike.objects.all()
