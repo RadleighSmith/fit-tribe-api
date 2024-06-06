0d591511a773394f51d40f211883fce6db0ddd14
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-06 19:06:00 +0000

    Add workout comment views

--- workout_comments/views.py
+++ workout_comments/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from ft_api.permissions import IsOwnerOrReadOnly
+from .models import WorkoutComment
+from .serializers import WorkoutCommentSerializer, WorkoutCommentDetailSerializer
 
-# Create your views here.
+class WorkoutCommentList(generics.ListCreateAPIView):
+    serializer_class = WorkoutCommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = WorkoutComment.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+        
+class WorkoutCommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = WorkoutCommentDetailSerializer
+    queryset = WorkoutComment.objects.all()
+    
