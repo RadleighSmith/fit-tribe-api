1d7eed1ab21846bc714cb63d09a462638d3a7b3b
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-06 19:44:21 +0000

    Add workout likes model

--- workout_likes/models.py
+++ workout_likes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from workouts.models import Workout
 
-# Create your models here.
+class WorkoutLike(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    workout = models.ForeignKey(
+        Workout, related_name='workout_likes', on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = (('owner', 'workout'),)
+        
+    def __str__(self):
+        return f'{self.owner.username} likes {self.workout.title}'
