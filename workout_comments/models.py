513b0438a27c7379482df81b96dfd74b42614c51
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-06 19:05:43 +0000

    Add workout comment model

--- workout_comments/models.py
+++ workout_comments/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from workouts.models import Workout
 
-# Create your models here.
+class WorkoutComment(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+        
+    def __str__(self):
+        return self.comment
