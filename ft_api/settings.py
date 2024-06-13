a963dfb6cd5ac32ef1a5263d5565a565be1e91c5
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-13 08:53:30 +0000

    Changed Allowed_hosts to environment variable

--- ft_api/settings.py
+++ ft_api/settings.py
@@ -62,7 +62,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'fittribe-api.herokuapp.com']
+ALLOWED_HOSTS = [
+    'localhost',
+    os.environ.get('ALLOWED_HOST'),
+    '127.0.0.1']
 
 
 # Application definition
