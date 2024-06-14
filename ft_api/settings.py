fa1503aef72b88700b744e32238404f4e7a3043a
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-14 19:20:38 +0000

    pdate settings to disable email verification while retaining email collection

--- ft_api/settings.py
+++ ft_api/settings.py
@@ -227,3 +227,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCOUNT_EMAIL_VERIFICATION = "none"
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_AUTHENTICATION_METHOD = "username"
