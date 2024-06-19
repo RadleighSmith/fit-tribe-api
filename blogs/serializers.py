6f5418cd12e274eb3cd5cd0b510627da221b5f50
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-19 11:33:48 +0000

    feat: Handle image removal in Blog model and serializer

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -131,6 +131,17 @@ class BlogSerializer(serializers.ModelSerializer):
             return like.id if like else None
         return None
 
+    def update(self, instance, validated_data):
+        remove_banner = validated_data.pop('remove_banner', False)
+        remove_image = validated_data.pop('remove_image', False)
+
+        if remove_banner:
+            instance.banner = 'default_blog_banner_t4cat3'  # Set to default banner
+        if remove_image:
+            instance.image = ''  # Remove the image
+        
+        return super().update(instance, validated_data)
+
     class Meta:
         model = Blog
         fields = [
@@ -138,5 +149,5 @@ class BlogSerializer(serializers.ModelSerializer):
             'profile_image', 'title', 'content', 
             'created_at', 'updated_at', 'banner', 
             'image', 'blog_like_id', 'blog_likes_count',
-            'blog_comments_count'
+            'blog_comments_count', 'remove_banner', 'remove_image'
         ]
