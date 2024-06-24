c7fbdc128e8f2703b1920deabb2b01b99560453d
Radleigh Smith <RadleighMSmith@gmail.com>
2024-06-24 19:16:34 +0000

    docs: Improve docstring and PEP8 compliance for CurrentUserSerializer and custom permissions files

--- ft_api/permissions.py
+++ ft_api/permissions.py
@@ -1,19 +1,23 @@
 from rest_framework import permissions
 
-from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
 
-    This permission class allows read-only access to any request (GET, HEAD, OPTIONS).
-    Write permissions (POST, PUT, DELETE) are only allowed to the owner of the object.
+    This permission class allows read-only access to any request
+    (GET, HEAD, OPTIONS). Write permissions (POST, PUT, DELETE)
+    are only allowed to the owner of the object.
 
     Methods:
-        has_object_permission(request, view, obj): Determines if the request has permission to perform the action on the given object.
-            - Allows read-only access for any request method listed in SAFE_METHODS (GET, HEAD, OPTIONS).
-            - Allows write access only if the user making the request is the owner of the object.
+        has_object_permission(request, view, obj): Determines if the request
+        has permission to perform the action on the given object.
+            - Allows read-only access for any request method listed in
+              SAFE_METHODS (GET, HEAD, OPTIONS).
+            - Allows write access only if the user making the request
+              is the owner of the object.
     """
+
     def has_object_permission(self, request, view, obj):
         # SAFE_METHODS are GET, HEAD or OPTIONS requests
         if request.method in permissions.SAFE_METHODS:
@@ -21,23 +25,33 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         # Write permissions are only allowed to the owner of the object
         return obj.owner == request.user
 
-    
+
 class IsAdminOrReadOnly(permissions.BasePermission):
     """
-    Custom permission to only allow admin users (staff or superuser) to edit objects.
+    Custom permission to only allow admin users (staff or superuser)
+    to edit objects.
 
-    This permission class allows read-only access to any request (GET, HEAD, OPTIONS).
-    Write permissions (POST, PUT, DELETE) are only allowed to admin users, which are defined as users
-    who have either the `is_staff` or `is_superuser` attribute set to True.
+    This permission class allows read-only access to any request
+    (GET, HEAD, OPTIONS). Write permissions (POST, PUT, DELETE)
+    are only allowed to admin users, which are defined as users who
+    have either the `is_staff` or `is_superuser` attribute set to True.
 
     Methods:
-        has_permission(request, view): Determines if the request has permission to perform the action.
-            - Allows read-only access for any request method listed in SAFE_METHODS (GET, HEAD, OPTIONS).
-            - Allows write access only if the user making the request is either a staff member or a superuser.
+        has_permission(request, view): Determines if the request has permission
+        to perform the action.
+            - Allows read-only access for any request method listed in
+              SAFE_METHODS (GET, HEAD, OPTIONS).
+            - Allows write access only if the user making the request
+              is either a staff member or a superuser.
     """
+
     def has_permission(self, request, view):
         # SAFE_METHODS are GET, HEAD or OPTIONS requests
         if request.method in permissions.SAFE_METHODS:
             return True
-        # Write permissions are only allowed to admin users (either staff or superuser)
-        return request.user and (request.user.is_staff or request.user.is_superuser)
+        # Write permissions are only allowed to admin users
+        # (either staff or superuser)
+        is_admin = request.user and (
+            request.user.is_staff or request.user.is_superuser
+        )
+        return is_admin
--- ft_api/serializers.py
+++ ft_api/serializers.py
@@ -3,8 +3,31 @@ from rest_framework import serializers
 
 
 class CurrentUserSerializer(UserDetailsSerializer):
+    """
+    Serializer for the current user's details, extending the default
+    UserDetailsSerializer provided by dj_rest_auth to include additional
+    profile-related fields.
+
+    This serializer adds the following fields:
+    - profile_id: The ID of the user's profile.
+    - profile_image: The URL of the user's profile image.
+
+    Attributes:
+    - profile_id (ReadOnlyField): The ID of the user's profile, sourced from
+                                  the related profile object.
+    - profile_image (ReadOnlyField): The URL of the user's profile image,
+                                     sourced from the related profile object.
+
+    Meta:
+    - fields (tuple): The fields to be included in the serialized
+                      representation of the user, combining the default
+                      fields from UserDetailsSerializer with the added
+                      profile_id and profile_image fields.
+    """
     profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_image = serializers.ReadOnlyField(source='profile.profile_image.url')
+    profile_image = serializers.ReadOnlyField(
+        source='profile.profile_image.url'
+    )
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
